ab7a67161f086988275b33ab1cdf72c643fb96e5
maythushar-music <maythusharofficial@gmail.com>
2025-02-16 10:21:53 +0630

    Update ~reaction.py

--- nexichat/modules/~reaction.py
+++ nexichat/modules/~reaction.py
@@ -1,10 +1,14 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+import random
 from nexichat import nexichat
+
+EMOJIS = ["😍", "👍", "❤️", "💋", "🤣", "🥰", "🥺", "🥹", "😭", "🎃", "🫡", "😘", "🙃", "🙂", "🥲", "😁", "🤌", "🌚", "🍓", "🍭", "🍬", "🍼", "🗿", "🩵", "💜", "🤍", "💚", "💛", "🩷", "💙", "🖤", "💔", "🧡", "❤️‍🔥"]
 
 @nexichat.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
     try:
-        await message.react("👍")
+        random_emoji = random.choice(EMOJIS)
+        await message.react(random_emoji)
     except Exception as e:
         print(f"Failed to react to message: {e}")
